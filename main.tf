5df293f50d2aadd4bbcb8fcbd94278333d1be861
Deepulucky <deepikathadi07@gmail.com>
2023-12-07 17:12:56 +0530

    add

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ resource "aws_ssm_parameter" "params" {
 
 variable "params" {
   default = [
-    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopsd7.online/", type = "String" },
-    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopsd7.online/", type = "String" },
-    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopsd7.online/", type = "String" },
-    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopsd7.online/", type = "String" },
-    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopsd7.online/", type = "String" },
+    { name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopsd7.online:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopsd7.online:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopsd7.online:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopsd7.online:8080/", type = "String" },
+    { name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopsd7.online:8080/", type = "String" },
 
     { name = "roboshop.dev.cart.redis_host", value = "redis-dev.devopsd7.online", type = "String" },
     { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.devopsd7.online", type = "String" },
