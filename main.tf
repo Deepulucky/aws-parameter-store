ed5006bed98de10e8397a7cefddd6836698f22d7
Deepulucky <deepikathadi07@gmail.com>
2023-12-08 10:35:33 +0530

    port

--- main.tf
+++ main.tf
@@ -17,15 +17,15 @@ variable "params" {
 
     { name = "roboshop.dev.cart.redis_host", value = "redis-dev.devopsd7.online", type = "String" },
     { name = "roboshop.dev.cart.catalogue_host", value = "catalogue-dev.devopsd7.online", type = "String" },
-    { name = "roboshop.dev.cart.catalogue_port", value = "80", type = "String" },
+    { name = "roboshop.dev.cart.catalogue_port", value = "8080", type = "String" },
 
     { name = "roboshop.dev.catalogue.mongo", value = "true", type = "String" },
     { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://mongodb-dev.devopsd7.online:27017/catalogue", type = "String" },
     
     { name = "roboshop.dev.payment.cart_host", value = "cart-dev.devopsd7.online", type = "String" },
-    { name = "roboshop.dev.payment.cart_port", value = "80", type = "String" },
+    { name = "roboshop.dev.payment.cart_port", value = "8080", type = "String" },
     { name = "roboshop.dev.payment.user_host", value = "user-dev.devopsd7.online", type = "String" },
-    { name = "roboshop.dev.payment.user_port", value = "80", type = "String" },
+    { name = "roboshop.dev.payment.user_port", value = "8080", type = "String" },
     { name = "roboshop.dev.payment.amqp_host", value = "rabbitmq-dev.devopsd7.online", type = "String" },
     { name = "roboshop.dev.payment.amqp_user", value = "roboshop", type = "String" },
     { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.devopsd7.online:80", type = "String" },
