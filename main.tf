ace1134259df8eafb4f78559f7e70099fe575c59
Deepulucky <deepikathadi07@gmail.com>
2023-12-08 11:24:56 +0530

    port

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ variable "params" {
     { name = "roboshop.dev.payment.user_port", value = "8080", type = "String" },
     { name = "roboshop.dev.payment.amqp_host", value = "rabbitmq-dev.devopsd7.online", type = "String" },
     { name = "roboshop.dev.payment.amqp_user", value = "roboshop", type = "String" },
-    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.devopsd7.online:80", type = "String" },
+    { name = "roboshop.dev.shipping.cart_endpoint", value = "cart-dev.devopsd7.online:8080", type = "String" },
     { name = "roboshop.dev.shipping.db_host", value = "mysql-dev.devopsd7.online", type = "String" },
     { name = "roboshop.dev.user.mongo", value = "true", type = "String" },
     { name = "roboshop.dev.user.redis_host", value = "redis-dev.devopsd7.online", type = "String" },
@@ -41,7 +41,7 @@ variable "params" {
     ### Passwords will be never part of git repos, Usually in organizations we create them nanually. Meaning whom ever have access they will provision these secrets in parameter store.
     { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
     { name = "roboshop.dev.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
-    { name = "roboshop.dev.mysql.password", value = "roboshop123", type = "SecureString" },
+    //{ name = "roboshop.dev.mysql.password", value = "roboshop123", type = "SecureString" },
    // { name = "roboshop.dev.docdb.password", value = "roboshop123", type = "SecureString" },
    // { name = "roboshop.dev.catalogue.mongo_url", value = "mongodb://roboshop:roboshop123@docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false", type = "SecureString" },
     //{ name = "roboshop.dev.catalogue.docdb_endpoint", value = "docdb-dev.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com", type = "String" },
